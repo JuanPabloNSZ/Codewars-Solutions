b6483488d5d97ec9c62bd3edc29f704592f0d672
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-03-31 20:49:13 -0300

    Update keepupthehoop.js

--- keepupthehoop.js
+++ keepupthehoop.js
@@ -5,10 +5,17 @@
 // If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
 // If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 
+//* This was my previous solution
+// function hoopCount(n) {
+// 	if (n >= 10) {
+// 		return 'Great, now move on to tricks';
+// 	} else {
+// 		return 'Keep at it until you get it';
+// 	}
+// }
+
 function hoopCount(n) {
-	if (n >= 10) {
-		return 'Great, now move on to tricks';
-	} else {
-		return 'Keep at it until you get it';
-	}
+	return n >= 10
+		? 'Great, now move on to tricks'
+		: 'Keep at it until you get it';
 }
