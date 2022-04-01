9ce88b1d13f494f64c0038767e1d1f16fa4d8b6b
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-04-01 20:06:29 -0300

    Update howgoodareyoureally.js

--- howgoodareyoureally.js
+++ howgoodareyoureally.js
@@ -1,4 +1,3 @@
-//* Details:
 // There was a test in your class and you passed it. Congratulations!
 // But you're an ambitious person. You want to know if you're better than the average student in your class.
 
