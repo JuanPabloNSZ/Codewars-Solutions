93e22c42bb96535275fda5111445b2dc9561534e
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-04-17 20:47:16 -0400

    Update lost_without_a_map.js

--- lost_without_a_map.js
+++ lost_without_a_map.js
@@ -1,8 +1,6 @@
 // Given an array of integers, return a new array with each value doubled.
 
 function maps(x) {
-	let doubles = x.map((n) => {
-		return n * 2;
-	});
+	let doubles = x.map((n) => n * 2);
 	return doubles;
 }
