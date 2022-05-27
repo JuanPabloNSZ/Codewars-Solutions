4bb942d06684442fa6f198df1c73e702fd16f4cf
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-05-26 23:27:59 -0400

    Update gradeBook.js

--- gradeBook.js
+++ gradeBook.js
@@ -9,6 +9,27 @@
 
 function getGrade(s1, s2, s3) {
 	let average = (s1 + s2 + s3) / 3;
+	switch (true) {
+		case average >= 90 && average <= 100:
+			return 'A';
+			break;
+		case average >= 80 && average < 90:
+			return 'B';
+			break;
+		case average >= 70 && average < 80:
+			return 'C';
+			break;
+		case average >= 60 && average < 70:
+			return 'D';
+			break;
+		case average >= 0 && average < 60:
+			return 'F';
+			break;
+	}
+}
+
+function getGrade(s1, s2, s3) {
+	let average = (s1 + s2 + s3) / 3;
 	if (average >= 90 && average <= 100) {
 		return 'A';
 	} else if (average >= 80 && average < 90) {
