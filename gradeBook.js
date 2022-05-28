4d47c14451d10d1a956079bc06b74ef30182745a
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-05-27 20:39:33 -0400

    Update gradeBook.js

--- gradeBook.js
+++ gradeBook.js
@@ -1,12 +1,13 @@
-// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+/* Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
 
-// Numerical Score	Letter Grade
-// 90 <= score <= 100   'A'
-// 80 <= score < 90     'B'
-// 70 <= score < 80     'C'
-// 60 <= score < 70     'D'
-// 0 <= score < 60      'F'
+Numerical Score	Letter Grade
+90 <= score <= 100   'A'
+80 <= score < 90     'B'
+70 <= score < 80     'C'
+60 <= score < 70     'D'
+0 <= score < 60      'F' */
 
+//* New Solution
 function getGrade(s1, s2, s3) {
 	let average = (s1 + s2 + s3) / 3;
 	switch (true) {
@@ -28,7 +29,8 @@ function getGrade(s1, s2, s3) {
 	}
 }
 
-function getGrade(s1, s2, s3) {
+//* Old Solution
+/* function getGrade(s1, s2, s3) {
 	let average = (s1 + s2 + s3) / 3;
 	if (average >= 90 && average <= 100) {
 		return 'A';
@@ -41,4 +43,4 @@ function getGrade(s1, s2, s3) {
 	} else if (average >= 0 && average < 60) {
 		return 'F';
 	}
-}
+} */
