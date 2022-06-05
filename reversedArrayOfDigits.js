9f798ee1f5cc1c4ac238c9347f9956ac05f1fcef
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-06-04 21:03:39 -0400

    Update reversedArrayOfDigits.js

--- reversedArrayOfDigits.js
+++ reversedArrayOfDigits.js
@@ -1,5 +1,6 @@
 /* Given a random non-negative number, you have to return the digits of this number within an array in reverse order. */
 
+// My Solution:
 function digitize(n) {
 	//code here
 	let nArray = Array.from(String(n), Number);
