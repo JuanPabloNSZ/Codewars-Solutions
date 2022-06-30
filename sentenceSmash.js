eaa14c71d899bdca63b5de7a41dc385a9dbc739a
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-06-29 20:07:37 -0400

    Create sentenceSmash.js

---
+++ sentenceSmash.js
@@ -0,0 +1,6 @@
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence.
+
+// My Solution:
+function smash(words) {
+	return words.join(' ');
+}
