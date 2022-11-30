7b4c2a59e2feffc6d17699ba2899f83f83cf9889
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-11-29 23:30:07 -0300

    Create jennysSecretMessage.js

---
+++ jennysSecretMessage.js
@@ -0,0 +1,14 @@
+/* Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+Can you help her? */
+
+// Original code:
+function greet(name) {
+	return 'Hello, ' + name + '!';
+	if (name === 'Johnny') return 'Hello, my love!';
+}
+
+//* My solution:
+function greet(name) {
+	return name === 'Johnny' ? 'Hello, my love!' : 'Hello, ' + name + '!';
+}
