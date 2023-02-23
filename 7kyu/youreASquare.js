8d9e4ea019b696f809d1e9743f8f3c60bc2df8b7
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2023-02-23 00:00:29 -0300

    Update youreASquare.js

--- 7kyu/youreASquare.js
+++ 7kyu/youreASquare.js
@@ -23,7 +23,7 @@ let isSquare = function (n) {
 	return Number.isInteger(Math.sqrt(n));
 };
 
-//* My solution previous solution:
+//* My previous solution:
 // let isSquare = function (n) {
 // 	if (n < 0) return false;
 // 	else if (Math.sqrt(n) === Math.floor(Math.sqrt(n))) return true;
