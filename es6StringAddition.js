6c54ba145cd47decae08985842589e46e70af1ca
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2023-03-11 23:39:30 -0300

    Create es6StringAddition.js

---
+++ es6StringAddition.js
@@ -0,0 +1,10 @@
+/* Your task is to find out what this is and write a function that will add two strings together, there must be a space between the two strings.
+
++ , .concat() & .join() will not be allowed in this exercise. I have also removed some other methods that can be used to cheat!
+
+If one of the arguments is a number your code must coerce it into being a string. */
+
+//* My solution:
+function joinStrings(string1, string2) {
+	return `${string1} ${string2}`;
+}
