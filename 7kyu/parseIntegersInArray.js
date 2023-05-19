af27457974148ee0f17d03621853165c5589bc19
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2023-05-18 22:53:09 -0400

    Create parseIntegersInArray.js

---
+++ 7kyu/parseIntegersInArray.js
@@ -0,0 +1,11 @@
+/* A colleague asked if you can help him fix his function. It seems it doesn't always parse Integers correctly.
+
+Examples:
+
+parseNumbers(['10']) // should return [10]
+parseNumbers(['-1','0','1']) // should return [-1,0,1] */
+
+//* My solution:
+var parseNumbers = function (intStrs) {
+  return intStrs.map((x) => parseInt(x));
+};
