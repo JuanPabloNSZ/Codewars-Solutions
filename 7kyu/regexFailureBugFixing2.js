97e92210799afc801d3f3bec1dcc8079f50fb2a7
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2023-05-29 22:17:29 -0400

    Create regexFailureBugFixing2.js

---
+++ 7kyu/regexFailureBugFixing2.js
@@ -0,0 +1,10 @@
+/* Oh no, Timmy's received some hate mail recently but he knows better. Help Timmy fix his regex filter so he can be awesome again! */
+
+// function filterWords(phrase){
+//   return phrase.replace(/(bad|mean|ugly|horrible|hideous)/,'awesome');
+// }
+
+//* My solution:
+function filterWords(phrase) {
+  return phrase.replace(/(bad|mean|ugly|horrible|hideous)/gi, "awesome");
+}
