ce928b99dff85b97752c38f0aa90fb8f150f826e
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2023-06-30 21:22:42 -0400

    Update maxHeadroom.js

--- maxHeadroom.js
+++ maxHeadroom.js
@@ -1,16 +1,19 @@
 /* Shouldn't the two functions getMax1 and getMax2 be equivalent and return the same value? Can you spot a problem and fix it? */
 
 //* My solution:
-function getMax1() {
-  let max1 = {
-    name: "Max Headroom",
-  };
-  return max1;
-}
+const getMax1 = (getMax2 = () => ({ name: "Max Headroom" }));
 
-function getMax2() {
-  let max2 = {
-    name: "Max Headroom",
-  };
-  return max2;
-}
+// My old solution:
+// function getMax1() {
+//   let max1 = {
+//     name: "Max Headroom",
+//   };
+//   return max1;
+// }
+
+// function getMax2() {
+//   let max2 = {
+//     name: "Max Headroom",
+//   };
+//   return max2;
+// }
