6bdadd738168a343c6762ab33f70d24b94b52d96
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2023-07-27 20:54:01 -0400

    Create extendingJavaScriptObjectsReverseString.js

---
+++ 7kyu/extendingJavaScriptObjectsReverseString.js
@@ -0,0 +1,14 @@
+/* Unfortunately, there's no a .reverse() method for the JavaScript String object.
+
+Your task is to extend JavaScript String object, so you can reverse strings just like this:
+
+'Hello, World!'.reverse();
+
+The method should return:
+'!dlroW ,olleH'
+*/
+
+//* My solution:
+String.prototype.reverse = function () {
+  return this.split("").reverse().join("");
+};
