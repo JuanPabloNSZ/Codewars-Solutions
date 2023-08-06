ebfdf4f893f3b74e99ad771dbc35175018492f60
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2023-08-05 23:16:21 -0400

    Create addPropertyToEveryObjectInArray.js

---
+++ 7kyu/addPropertyToEveryObjectInArray.js
@@ -0,0 +1,4 @@
+/* Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length. */
+
+//* My solution:
+questions.map((x) => (x.usersAnswer = null));
